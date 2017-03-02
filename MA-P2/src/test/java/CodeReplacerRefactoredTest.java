9ceeceeb43fe7ca8e2a58026c2efa8bcbedbf510
SureSRM <sergiozz95@gmail.com>
2017-03-02 15:33:11 +0100

    Real test created

---
+++ MA-P2/src/test/java/CodeReplacerRefactoredTest.java
@@ -0,0 +1,14 @@
+package java;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by sergio on 2/03/17.
+ */
+public class CodeReplacerRefactoredTest {
+    @org.junit.Test
+    public void substitute() throws Exception {
+
+    }
+
+}
