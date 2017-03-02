7aefcc293f8f14765227cf89ee029164e2ef1275
SureSRM <sergiozz95@gmail.com>
2017-03-02 16:32:27 +0100

    Add new tests and deletes pre-generates template

--- MA-P2/src/test/P2/CodeReplacerRefactoredTest.java
+++ MA-P2/src/test/P2/CodeReplacerRefactoredTest.java
@@ -1,16 +1,84 @@
 package P2;
 
-import java.io.PrintWriter;
+import org.junit.*;
 
-/**
- * Created by sergio on 2/03/17.
- */
+import java.io.*;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+
 public class CodeReplacerRefactoredTest {
-    @org.junit.Test
-    public void substitute() throws Exception {
-        CodeReplacerRefactored c = new CodeReplacerRefactored();
-        c.substitute("", new PrintWriter( System.out) );
+
+    private static CodeReplacerRefactored c;
+    private static ByteArrayOutputStream file;
+    private static PrintWriter out;
+
+    @BeforeClass
+    public static void beforeClass(){
+        c = new CodeReplacerRefactored();
+        File template = new File("template.html");
+//        template.deleteOnExit();
+
+        try {
+            template.createNewFile();
+            BufferedWriter out = new BufferedWriter(new FileWriter(template));
+            out.write("%NAME%\n%INITIALS%");
+            out.flush();
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
+
+    @Before
+    public void before(){
+        file = new ByteArrayOutputStream();
+        out = new PrintWriter(file);
+    }
+
+    @After
+    public void after(){
+        out.close();
+    }
+
+    @Test
+    public void substitute() throws Exception {
+        c.substitute("Sergio Martin", out );
+        assertEquals("Sergio Martin\nSM\n",file.toString());
+    }
+
+    @Test
+    public void substitute2() throws Exception {
+        c.substitute("", out );
+        assertEquals("\n\n",file.toString());
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void substitute3() throws Exception {
+        c.substitute("", null );
+        assertEquals("\n\n",file.toString());
+    }
+
+    @Test
+    public void substitute4() throws Exception {
+        c.substitute(null, out );
+        assertEquals("",file.toString());
+    }
+
+    @Test
+    public void substitute5() throws Exception {
+        c.substitute("1", out );
+        assertEquals("1\n1\n",file.toString());
+    }
+
+    @Test
+    public void substitute6() throws Exception {
+        c.substitute("1 2", out );
+        assertEquals("1 2\n12\n",file.toString());
+    }
+
+
 
 }
